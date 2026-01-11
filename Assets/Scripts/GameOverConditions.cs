8b653a308dc2cb7e7937b0a9443899884458b1a9
blueberry <ansisiddhu@gmail.com>
2026-01-11 21:13:14 +0530

    Add mom audio sequences and sound effects for game over
    Introduced new audio clips and GameObjects for mom's voice lines and reactions during game over conditions. Updated GameOverConditions to play specific audio sequences and visual effects based on player state (PC on, eyes open, not sleeping). Added PlayAndWait coroutine to SoundFXManager for synchronized audio playback. Adjusted SampleScene to reference new audio assets and tweaked sound distance parameters.

--- Assets/Scripts/GameOverConditions.cs
+++ Assets/Scripts/GameOverConditions.cs
@@ -1,21 +1,119 @@
 using UnityEngine;
+using System.Collections;
 
 public class GameOverConditions : MonoBehaviour
 {
     public SleepAnimTrig sleep;
     public EyesScript eyes;
+    public GameObject momPart1;
+    public GameObject momPart2;
+    Coroutine flickerRoutine;
+    float flickerSpeed = 0.15f;
+    [SerializeField] AudioClip whyPcOnPart1;
+    [SerializeField] AudioClip whyPcOnPart2;
+    [SerializeField] AudioClip whyEyesOpenPart1;
+    [SerializeField] AudioClip whyEyesOpenPart2;
+    [SerializeField] AudioClip whyNoSleep;
 
     public bool PlayerIsSafe()
     {
         if (TurnPcOn.PcIsOn)
         {
+            StartCoroutine(PcOnSequence());
             return false;
         }
-        else if (!sleep.sleeping || !eyes.leftEye.activeSelf || !eyes.rightEye.activeSelf)
+
+        if (sleep.sleeping && (!eyes.leftEye.activeSelf || !eyes.rightEye.activeSelf))
         {
-            Debug.Log("// why are u awake samantha");
+            Debug.Log("why are u awake samantha");
+            StartCoroutine(WhyEyesOpenSequence());
             return false;
         }
+
+        if (!sleep.sleeping)
+        {
+            StartCoroutine(NoSleepSequence());
+            return false;
+        }
+
         return true;
     }
+
+    bool pcSequenceRunning = false;
+    bool eyeSequenceRunning = false;
+    bool noSleepRunning = false;
+
+    IEnumerator PcOnSequence()
+    {
+        if (pcSequenceRunning) yield break;
+        pcSequenceRunning = true;
+
+        // PART 1
+        yield return SoundFXManager.instance.PlayAndWait(whyPcOnPart1, transform, 1f);
+
+        momPart1.SetActive(false);
+        momPart2.SetActive(true);
+
+        // PART 2
+        yield return SoundFXManager.instance.PlayAndWait(whyPcOnPart2, transform, 1f);
+
+        momPart2.SetActive(false);
+        momPart1.SetActive(true);
+
+        pcSequenceRunning = false;
+    }
+
+    IEnumerator WhyEyesOpenSequence()
+    {
+        if (eyeSequenceRunning) yield break;
+        eyeSequenceRunning = true;
+
+        // PART 1
+        yield return SoundFXManager.instance.PlayAndWait(whyEyesOpenPart1, transform, 1f);
+
+        momPart1.SetActive(false);
+        momPart2.SetActive(true);
+
+        // PART 2
+        yield return SoundFXManager.instance.PlayAndWait(whyEyesOpenPart2, transform, 1f);
+
+        momPart2.SetActive(false);
+        momPart1.SetActive(true);
+
+        eyeSequenceRunning = false;
+    }
+
+    IEnumerator NoSleepSequence()
+    {
+        if (noSleepRunning) yield break;
+        noSleepRunning = true;
+
+        // Start flickering mom while audio plays
+        flickerRoutine = StartCoroutine(MomFlicker());
+
+        yield return SoundFXManager.instance.PlayAndWait(whyNoSleep, transform, 1f);
+
+        // Stop flicker and restore mom
+        if (flickerRoutine != null)
+            StopCoroutine(flickerRoutine);
+
+        momPart2.SetActive(false);
+        momPart1.SetActive(true);
+
+        noSleepRunning = false;
+    }
+    IEnumerator MomFlicker()
+    {
+        while (true)
+        {
+            momPart1.SetActive(false);
+            momPart2.SetActive(true);
+            yield return new WaitForSeconds(flickerSpeed);
+
+            momPart2.SetActive(false);
+            momPart1.SetActive(true);
+            yield return new WaitForSeconds(flickerSpeed);
+        }
+    }
+
 }
--- Assets/Scripts/SoundFXManager.cs
+++ Assets/Scripts/SoundFXManager.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
-using UnityEngine.Rendering;
-
+using System.Collections;
 public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager instance;
     [SerializeField] private AudioSource soundFXObject;
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
         }
     }
-
     public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
     {
         //spawn in gameObject;
@@ -32,6 +30,19 @@ public class SoundFXManager : MonoBehaviour
 
         //destroy the clip after it is done playing
         Destroy(audioSource.gameObject, clipLength);
-        
+
     }
+
+    public IEnumerator PlayAndWait(AudioClip clip, Transform spawnTransform, float volume)
+    {
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        Destroy(audioSource.gameObject);
+    }
+
 }
